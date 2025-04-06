89976f82b65ab33bf3c7fe67e974a8ece09c3c63
HouriaNouhKhanjar <gls97048@students.codeinstitute.net>
2025-04-06 10:32:02 +0200

    prepare files to deploy the project

--- car_rental/settings.py
+++ car_rental/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)vz0e0r^h)cu6--^##$4ian2edzcf@ke1@shhs(e#q*zdpscg&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
